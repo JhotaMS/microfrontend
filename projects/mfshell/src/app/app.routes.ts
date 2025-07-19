4e2b58ed0a386a5851d3a8336e426da9e317636e
Jhonatan Alejandro Muñoz Serna <jhotams.developer@gmail.com>
2025-07-18 22:32:47 -0500

    Configuración de microfrontend basica exitosa

--- projects/mfshell/src/app/app.routes.ts
+++ projects/mfshell/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // Add this route:
+  {
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule('mftenantexampleuno', './Component').then((m) => m.AppComponent),
+  },
+
+  /*{
+    path: '**',
+    component: NotFoundComponent,
+  },
+  */
+];
